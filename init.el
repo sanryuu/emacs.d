afa823f9eb97c9035503a55ef8a2900597621f61
sanryuu <cpu.cpu.cpu9@gmail.com>
2013-05-20 00:44:29 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,15 @@
+(keyboard-translate ?\C-h ?\C-?)  ; translate `C-h' to DEL
+
+;; load-pathを追加する関数を定義
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+	      (expand-file-name (concat user-emacs-directory path))))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
+
+;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
+(add-to-load-path "elisp" "conf" "public_repos")
+
