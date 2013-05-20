9bba723441d342fd592618b86d6e20d1d4c2201e
sanryuu <cpu.cpu.cpu9@gmail.com>
2013-05-21 01:02:33 +0900

    install-elispの設定を記述

--- init.el
+++ init.el
@@ -13,3 +13,8 @@
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
 (add-to-load-path "elisp" "conf" "public_repos")
 
+
+;; まず、install-elisp のコマンドを使える様にします。
+(require 'install-elisp)
+;; 次に、Elisp ファイルをインストールする場所を指定します。
+(setq install-elisp-repository-directory "~/.emacs.d/elisp/")
